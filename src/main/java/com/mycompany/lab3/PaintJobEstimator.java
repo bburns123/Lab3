83f9267b722da15ffeb3a43298f74340cef8fc7d
Brendyn Burns <Brendyn Burns@DESKTOP-9USEOT4>
2019-09-15 17:48:11 -0500

    PaintJobEstimator last commit

--- src/main/java/com/mycompany/lab3/PaintJobEstimator.java
+++ src/main/java/com/mycompany/lab3/PaintJobEstimator.java
@@ -5,17 +5,37 @@
  */
 package com.mycompany.lab3;
 
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 /**
  *
- * @author Brendyn Burns
+ * @author Brendyn Burns (0800633)
  */
 public class PaintJobEstimator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the number of rooms: ");
-        int walls = scanner.nextInt() / 4;
-        System.out.println(walls);
+        int walls = scanner.nextInt() * 4;
+        System.out.println("Please enter the square footage of each wall: ");
+        int squareFeet = scanner.nextInt();
+        System.out.println("Please enter the price per gallon of paint: ");
+        double pricePerGallon = scanner.nextDouble();
+        
+        int totalSquareFeet = squareFeet * walls;
+        int gallonsOfPaint = totalSquareFeet / 115;
+        double totalCostOfPaint = pricePerGallon * gallonsOfPaint;
+        int totalAmountOfLabor = gallonsOfPaint * 8;
+        double totalCostOfLabor = totalAmountOfLabor * 18.00;
+        double totalCostOfJob = totalCostOfLabor + totalCostOfPaint;
+        
+        NumberFormat currencyInstance = NumberFormat.getCurrencyInstance();
+        
+        System.out.println("Number of gallons of paint required: " + gallonsOfPaint);
+        System.out.println("Hours of labor required: " + totalAmountOfLabor);
+        System.out.println("Cost of paint: " + currencyInstance.format(totalCostOfPaint));
+        System.out.println("Cost of labor: " + currencyInstance.format(totalCostOfLabor));
+        System.out.println("Total cost of paint job: " + currencyInstance.format(totalCostOfJob));
+        
     }
 }

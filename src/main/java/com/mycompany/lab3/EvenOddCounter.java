f2540d1a76c21b086d7622e6745b03e8eb7292ea
Brendyn Burns <Brendyn Burns@DESKTOP-9USEOT4>
2019-09-15 19:36:06 -0500

    EvenOddCounter first commit

---
+++ src/main/java/com/mycompany/lab3/EvenOddCounter.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab3;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ *
+ * @author Brendyn Burns
+ */
+public class EvenOddCounter {
+    static boolean isEven(int number) {
+        if ((number % 2) == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        Random r = new Random();
+        ArrayList<Integer> evenArray = new ArrayList<Integer>();
+        ArrayList<Integer> oddArray = new ArrayList<Integer>();
+        
+        for (int counter = 1; counter >= 100; counter++) {
+            int randomNumber = r.nextInt();
+            if (isEven(randomNumber)) {
+                evenArray.add(randomNumber);
+            }
+            else {
+                oddArray.add(randomNumber);
+            }
+        }
+        
+        System.out.println("The random even numbers are: \n" + evenArray);
+        System.out.println("The random odd numbers are: \n" + oddArray);
+        
+    }
+    
+}
